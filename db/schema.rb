3faf7c40614ff109a452d364217dd47d50ff9197
Deepanshu Aggarwal <aggarwaldeepanshu96@gmail.com>
2019-06-06 11:24:38 +0530

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_101512) do
+ActiveRecord::Schema.define(version: 2019_06_06_054305) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
